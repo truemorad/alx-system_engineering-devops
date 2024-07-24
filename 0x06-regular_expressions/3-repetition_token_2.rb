5dd90649e90023cabd61761f98e999555ce73af6
truemorad <truemorad@gmail.com>
2024-07-25 00:29:43 +0400

    m

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# Get the line from the argument
+line = ARGV[0]
+
+# Define a regular expression pattern to match the word "School"
+pattern = /hbt+n/
+
+# Find all matches in the line
+matches = line.scan(pattern)
+
+# Output each match
+  puts matches.join("")
