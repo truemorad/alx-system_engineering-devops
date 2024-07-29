17c4cf3e98730ff427cd1eded7653d617c0e6b82
truemorad <truemorad@gmail.com>
2024-07-29 16:08:18 +0400

    This

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+userId = sys.argv[1]
+source = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(userId))
+if source.status_code == 200:
+    name = source.json()
+
+EMPLOYEE_NAME = name.get('name')
+
+
+todos_url = requests.get('https://jsonplaceholder.typicode.com/todos')
+if todos_url.status_code == 200:
+    todos = todos_url.json()
+
+NUMBER_OF_DONE_TASKS = 0
+TOTAL_NUMBER_OF_TASKS = 0
+
+for task in todos:
+        if task.get('userId') == int(userId):
+            TOTAL_NUMBER_OF_TASKS += 1
+            if task.get('completed'):
+                NUMBER_OF_DONE_TASKS += 1
+
+print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+print('\n'.join(["\t " + task.get('title') for task in todos
+          if task.get('userId') == int(userId) and task.get('completed')]))
