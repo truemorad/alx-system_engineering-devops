e874fa5d721c50c78d080cd340a642150ffa3467
truemorad <truemorad@gmail.com>
2024-08-13 17:37:35 +0400

    autocommit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+# this is a script that queries the Reddit API and returns the number of subscribers for a given subreddit.
 import requests
 def number_of_subscribers(subreddit):
     api = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     response = requests.get(api, headers=headers)
-    if response.status_code == 100 or response.status_code == 200 or response.status_code == 201:
+    if response.status_code == 100 or response.status_code == 200:
         return response.json().get('data').get('subscribers')
     else:
         return 0
