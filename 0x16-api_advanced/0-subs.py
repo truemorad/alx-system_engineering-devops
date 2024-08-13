abb3920773f880bb300952c0f166c3e8701e5c2d
truemorad <truemorad@gmail.com>
2024-08-13 17:47:08 +0400

    autocommit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     api = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(api, headers=headers)
+    response = requests.get(api, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
     else:
