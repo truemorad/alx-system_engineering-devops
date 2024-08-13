ece6c6e94f178b1da6029c2b3a5ebdb302b87285
truemorad <truemorad@gmail.com>
2024-08-13 17:36:50 +0400

    autocommit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,8 +5,7 @@ def number_of_subscribers(subreddit):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     response = requests.get(api, headers=headers)
-    if response.status_code == 404:
-        return 0
-    else:
+    if response.status_code == 100 or response.status_code == 200 or response.status_code == 201:
         return response.json().get('data').get('subscribers')
-    
+    else:
+        return 0
