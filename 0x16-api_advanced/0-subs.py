44eed2635147f7891431943d26c8126f7689d393
truemorad <truemorad@gmail.com>
2024-08-13 18:48:25 +0400

    autocommit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     api = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(api, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    else:
+    if response.status_code == 200:
         return response.json().get('data').get('subscribers')
+    else:
+        return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,8 +9,8 @@ def top_ten(subreddit):
     api = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(api, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        print(None)
-    else:
+    if response.status_code == 200:
         for post in response.json().get('data').get('children'):
             print(post.get('data').get('title'))
+    else:
+        return None
