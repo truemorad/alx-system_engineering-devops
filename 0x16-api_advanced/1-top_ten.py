ec3836f32b1dd1c06dfbb96915ee0b2e9364ed93
truemorad <truemorad@gmail.com>
2024-08-13 18:07:51 +0400

    autocommit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+ a script that returns top ten hot title for a post.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    api = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(api, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        print(None)
+    else:
+        for post in response.json().get('data').get('children'):
+            print(post.get('data').get('title'))
