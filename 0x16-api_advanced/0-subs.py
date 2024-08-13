c2ee3aca94b5980c1a420a9230871e9926d400f9
truemorad <truemorad@gmail.com>
2024-08-13 19:51:36 +0400

    autocommit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,12 @@
 import requests
 
 
-def number_of_subscribers(subreddit):
-    api = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(api, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
-    else:
-        return 0
+if __name__ != '__main__':
+    def number_of_subscribers(subreddit):
+        api = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(api, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            return response.json().get('data').get('subscribers')
+        else:
+            return 0
