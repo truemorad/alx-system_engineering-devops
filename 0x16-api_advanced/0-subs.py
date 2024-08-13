132c28df6d098e7d0835eab5d298b1a384667a85
truemorad <truemorad@gmail.com>
2024-08-13 17:34:51 +0400

    autocommit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+import requests
 def number_of_subscribers(subreddit):
-    import requests
-    api =     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    api = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(api, headers=headers)
     if response.status_code == 404:
         return 0
-    else: 
-        return response.json().get('data').format('subscribers')
+    else:
+        return response.json().get('data').get('subscribers')
+    
